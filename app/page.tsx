861a3a2b8c612309f63a96fefccfe6b63bb2f0be
anglitj <jemanglit@gmail.com>
2025-03-28 08:21:31 +0800

    Adding components

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,103 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Button,
+  DarkThemeToggle,
+  Drawer,
+  DrawerHeader,
+  DrawerItems,
+  Sidebar,
+  SidebarItem,
+  SidebarItemGroup,
+  SidebarItems,
+} from "flowbite-react";
+import Link from "next/link";
+import { useState } from "react";
+import { GiHamburgerMenu, GiOpenTreasureChest } from "react-icons/gi";
+import { HiHome } from "react-icons/hi";
+import { IoRoseSharp } from "react-icons/io5";
+import { LiaMoneyBillWaveAltSolid } from "react-icons/lia";
+import { PiMoneyWavyFill } from "react-icons/pi";
+import { TfiGallery } from "react-icons/tfi";
+import { GiLargeDress } from "react-icons/gi";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [isOpen, setIsOpen] = useState(true);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleClose = () => setIsOpen(false);
+
+  return (
+    <main className="flex min-h-screen flex-col bg-white px-4 py-24 dark:bg-gray-900">
+      <div className="absolute top-4 right-4">
+        <DarkThemeToggle className="cursor-pointer" />
+      </div>
+      <div className="absolute top-4 left-4">
+        <Button
+          color="gray"
+          size="sm"
+          className="cursor-pointer rounded-full bg-gray-200 text-gray-800 hover:bg-gray-300 dark:bg-gray-600 dark:text-gray-900 dark:hover:bg-gray-700"
+          onClick={() => setIsOpen(true)}
+        >
+          <GiHamburgerMenu />
+        </Button>
+      </div>
+
+      <Drawer open={isOpen} onClose={handleClose}>
+        <DrawerHeader title="MENU" />
+        <DrawerItems>
+          <Sidebar>
+            <SidebarItems>
+              <SidebarItemGroup>
+                <SidebarItem href="/" icon={HiHome}>
+                  Home
+                </SidebarItem>
+
+                <SidebarItem href="/" icon={GiLargeDress}>
+                  Attire
+                </SidebarItem>
+                <SidebarItem href="/" icon={IoRoseSharp}>
+                  18 Roses
+                </SidebarItem>
+                <SidebarItem href="/" icon={GiOpenTreasureChest}>
+                  18 Treasures
+                </SidebarItem>
+                <SidebarItem href="/" icon={LiaMoneyBillWaveAltSolid}>
+                  18 Yellow Bills
+                </SidebarItem>
+                <SidebarItem href="/" icon={PiMoneyWavyFill}>
+                  18 Blue Bills
+                </SidebarItem>
+                <SidebarItem href="/" icon={TfiGallery}>
+                  Photo Gallery
+                </SidebarItem>
+              </SidebarItemGroup>
+            </SidebarItems>
+          </Sidebar>
+        </DrawerItems>
+      </Drawer>
+      <div className="flex h-full flex-col items-center justify-center gap-20">
+        <h3 className="text-center text-lg font-semibold text-gray-800 uppercase lg:text-2xl dark:text-gray-300">
+          Join us to celebrate
+        </h3>
+        <div className="flex flex-col items-center justify-center dark:text-gray-300">
+          <h1 className="text-4xl font-bold">Mae</h1>
+          <h3 className="text-4xl font-bold">at</h3>
+          <h1 className="text-4xl font-bold">18</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-col items-center justify-center font-semibold text-gray-800 md:text-lg lg:text-2xl dark:text-gray-300">
+          <p>March 27, 2025</p>
+          <p>10 PM</p>
+        </div>
+        <div className="cursor-pointer text-center font-semibold text-gray-800 uppercase underline hover:text-cyan-800 md:text-lg lg:text-2xl dark:text-gray-100 dark:hover:text-cyan-400">
+          <Link
+            href="https://www.google.com/maps/dir//Santa+Ana+rd,+Santo+Tomas,+Batangas/@14.0668232,121.1086801,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x33bd695dd883bd7f:0xca8ddd043881fa5e!2m2!1d121.1910819!2d14.0668372?entry=ttu&g_ep=EgoyMDI1MDMyNC4wIKXMDSoASAFQAw%3D%3D"
+            target="_blank"
+          >
+            <p>Jhoenie&apos;s Island Resort</p>
+            <p>Santa Ana Rd., Sto. Tomas, Batangas</p>
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
