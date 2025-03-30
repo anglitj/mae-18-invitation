89cb79143d04fe74a73c22d0398eb057cb166f0f
anglitj <jemanglit@gmail.com>
2025-03-30 20:44:12 +0800

    Refactor Home component: update layout structure to use a grid system, enhance text styling for event details, and improve accessibility with uppercase and capitalize text transformations.

--- app/page.tsx
+++ app/page.tsx
@@ -22,29 +22,40 @@ export default function Home() {
         />
 
         {/* Text */}
-        <div className="absolute inset-0 flex h-full flex-col items-center justify-center gap-20">
+
+        {/* <div className="absolute inset-0 grid h-full grid-rows-[35px_2fr_1fr_10rem] items-center justify-center gap-2"> */}
+        <div className="absolute inset-0 grid h-full grid-rows-[1fr_1.5fr_1fr_1fr] items-center justify-center">
+          {/* Title */}
           <h3 className="text-center text-lg font-semibold uppercase text-gray-800 dark:text-white lg:text-2xl">
             Join us to celebrate
           </h3>
+
+          {/* Name Center */}
           <div
-            className={`${imperialScript.className} flex flex-col items-center justify-center dark:text-white`}
+            className={`${imperialScript.className} flex flex-col items-center justify-center py-8 dark:text-white`}
           >
             <p className="-ml-20 text-9xl font-light">Mae</p>
             <p className="text-4xl font-semibold">at</p>
             <p className="ml-20 text-8xl font-light">18</p>
           </div>
+
+          {/* Date and Time */}
           <div className="flex flex-row items-center justify-center gap-4 font-semibold text-gray-900 dark:text-white md:text-lg lg:text-2xl">
             <p>April 20, 2025</p>
             <p>|</p>
             <p>5 PM</p>
           </div>
-          <div className="cursor-pointer text-center font-semibold uppercase text-gray-800 hover:text-gray-800 dark:text-gray-50 dark:hover:text-gray-400 md:text-lg lg:text-2xl">
+
+          {/* Location */}
+          <div className="cursor-pointer py-8 text-center font-semibold text-gray-800 hover:text-gray-800 dark:text-gray-50 dark:hover:text-gray-300 md:text-lg lg:text-2xl">
             <Link
               href="https://www.google.com/maps/dir//Santa+Ana+rd,+Santo+Tomas,+Batangas/@14.0668232,121.1086801,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x33bd695dd883bd7f:0xca8ddd043881fa5e!2m2!1d121.1910819!2d14.0668372?entry=ttu&g_ep=EgoyMDI1MDMyNC4wIKXMDSoASAFQAw%3D%3D"
               target="_blank"
             >
-              <p className="underline">Jhoenie&apos;s Island Resort</p>
-              <p>Santa Ana Rd., Sto. Tomas, Batangas</p>
+              <p className="uppercase underline">
+                Jhoenie&apos;s Island Resort
+              </p>
+              <p className="capitalize">Santa Ana Rd., Sto. Tomas, Batangas</p>
             </Link>
           </div>
         </div>
